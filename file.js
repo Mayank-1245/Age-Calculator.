d5095c8259d8e13347ac375d1ba2ae99359de97a
Mayank-1245 <69622316+Mayank-1245@users.noreply.github.com>
2020-09-28 14:19:52 +0530

    Update file.js
    initial commit

--- file.js
+++ file.js
@@ -4,7 +4,7 @@ function agecalculator(){
   let hrs = document.getElementById('age').value;
   let min = document.getElementById('age').value;
   let secs = document.getElementById('age').value;
-  Console.log(months * 12+' Months' + ' ' + '<br/>' + days * 12* 30+' Days' + ' ' + '<br/>'
+  document.write(months * 12+' Months' + ' ' + '<br/>' + days * 12* 30+' Days' + ' ' + '<br/>'
  + hrs * 12 * 30 * 24+' Hours'+ ' ' + '<br/>'
   + min * 12 * 30 * 24 * 60+' Minutes' +' ' + '<br/>'
   + secs * 12 * 30 * 24 * 60 * 60 +' Seconds');
